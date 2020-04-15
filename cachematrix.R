4d79376d39d13ffcf874764ee48519a6f989103d
Darya Papyan <63240151+dpapyan@users.noreply.github.com>
2020-04-15 13:58:57 +0300

    Add files via upload
    My cachematrix.R file has a code inside that was required by the Programming Assignment 2 on Coursera (as I was doing everything at once, I don't have other commits).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This code perpresents the ability of R to cache some data in order to save time on reloading the functions (in this code: matrix inversing)
 
-## Write a short comment describing this function
+# With the help of getters and setters function "makeCacheMatrix" creates and saves infrormation about the matrix input and it's inverse from the other function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() 
+      x
+    setinv <- function(inversion)
+      inv <<- inversion
+    getinv <- function()
+      inv
+    list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# This function takes infos in getters from the 1st function and, if they don't exist, inverses the created matrix and sends the result back to the 1st function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("your cached inversion:")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- Inverse(data, ...) # a function from a package "matlib"
+  x$setinv(inv)
+  inv
 }
